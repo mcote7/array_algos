987e322f3002c835d83e386b4a0ddcd16aa52d58
Michael Cote <michaelcote7@outlook.com>
2020-02-20 17:12:53 -0800

    "cotenew"

--- 3_reverse.js
+++ 3_reverse.js
@@ -3,11 +3,11 @@
 // When you have solved this, create new values for your array and ensure it still works as expected.
 
 // YOUR CODE HERE node 3_reverse.js
-
+           // length=7
 var arr = [8,-6,7,5,-3,0,9];
-var arrnew = [];
+var arrnew = [];                    // arrnew[9,0,-3,5,7,-6,8]!!!!!
 
-for(i=arr.length-1; i >= 0 ;i--){
+for(i=arr.length-1; i >= 0 ;i--){     // i = 6 - 5 - 4 - 3 - 2 - 1 - 0 - -1 Break
     arrnew.push(arr[i]);
 
 }
